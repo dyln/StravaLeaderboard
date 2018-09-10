7cd63d20c33d2c60a5cbf5531b7fd767d59a79e1
Caner Eren <caner.eren@milklab.com>
2018-09-11 00:51:56 +0300

    getting raw Segment data

--- test.py
+++ test.py
@@ -1,28 +1,22 @@
-#from __future__ import print_statement
-import time
-import swagger_client
-from swagger_client.rest import ApiException
-from pprint import pprint
+import json
+import urllib2
 
-# Configure OAuth2 access token for authorization: strava_oauth
-swagger_client.configuration.access_token = '418172ed16941e70d4ad096e00315d49260aaa3a'
+access_token = '418172ed16941e70d4ad096e00315d49260aaa3a'
 
-# create an instance of the API class
-api_instance = swagger_client.SegmentsApi()
-id = 789 # Long | The identifier of the segment leaderboard.
-gender = gender_example # String | Filter by gender. (optional)
-ageGroup = ageGroup_example # String | Summit Feature. Filter by age group. (optional)
-weightClass = weightClass_example # String | Summit Feature. Filter by weight class. (optional)
-following = true # Boolean | Filter by friends of the authenticated athlete. (optional)
-clubId = 789 # Long | Filter by club. (optional)
-dateRange = dateRange_example # String | Filter by date range. (optional)
-contextEntries = 56 # Integer |  (optional)
-page = 56 # Integer | Page number. (optional)
-perPage = 56 # Integer | Number of items per page. Defaults to 30. (optional) (default to 30)
+#The latitude and longitude for two points describing a rectangular boundary for the search: 
+#[southwest corner latitutde,
+#southwest corner longitude, 
+#northeast corner latitude, 
+#northeast corner longitude]
+#ISTANBUL
+bounds = [41.031471, 28.007190, 41.141815, 29.913041]
+url = 'https://www.strava.com/api/v3/segments/explore?bounds=41.031471,28.007190,41.141815,29.913041&activity_type=riding&access_token=418172ed16941e70d4ad096e00315d49260aaa3a'
 
-try: 
-    # Get Segment Leaderboard
-    api_response = api_instance.getLeaderboardBySegmentId(id, gender=gender, ageGroup=ageGroup, weightClass=weightClass, following=following, clubId=clubId, dateRange=dateRange, contextEntries=contextEntries, page=page, perPage=perPage)
-    pprint(api_response)
-except ApiException as e:
-    print("Exception when calling SegmentsApi->getLeaderboardBySegmentId: %s\n" % e)
+#http = urllib3.PoolManager()
+#response = http.request('GET', url)
+
+json_obj = urllib2.urlopen(url)
+
+data = json.load(json_obj)
+
+print (data[])
